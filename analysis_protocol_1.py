996019e6ae4095794a449c12d4fee6983c3cb7e8
menashe soffer <menashe.soffer.23@gmail.com>
2025-01-28 22:27:46 +0200

    pearson (need refignment)

--- analysis_protocol_1.py
+++ analysis_protocol_1.py
@@ -64,7 +64,7 @@ def make_data_availability_list(base_folder, region_list, hemisphere_sel):
     writer.close()
 
 
-def calc_contdwn_responces(subject, region=None, band=[40, 100], timebin_sec=0.5, avg=6, in_session_gap=13, scan_files_only=True):
+def calc_contdwn_responces(subject, regions=None, sides=None, band=[40, 100], timebin_sec=0.5, avg=6, in_session_gap=13, mode='monopolar', scan_files_only=True):
 
     # configurations for the sub=band processor
     # generate the sub-band spec for the processing
@@ -74,15 +74,16 @@ def calc_contdwn_responces(subject, region=None, band=[40, 100], timebin_sec=0.5
     # result holders
     p_list, sem_list, desc_list = [], [], []
 
-    paths = get_paths(base_folder=base_folder, subject=subject, sess_slct=None, mode='bipolar')
+    paths = get_paths(base_folder=base_folder, subject=subject, sess_slct=None, mode=mode)
     if len(paths) == 0:
         rc = subject + ':   no paths'
         return rc, None
 
     montage = my_montage_reader(fname=paths[0]['electrodes'])
-    electrode_list = montage.get_electrode_list_by_region(region_list=[region[0]], hemisphere_sel=[region[1]])
-    group = list(electrode_list.keys())[0]
-    contact_list = electrode_list[group]
+    electrode_list = montage.get_electrode_list_by_region(region_list=regions, hemisphere_sel=sides)
+    contact_list = []
+    for group in electrode_list:
+     contact_list = contact_list + electrode_list[group]
     num_contacts = len(contact_list)
     if num_contacts < 4:
         rc = 'not enough contacts for {} {}'.format(subject, group)
@@ -93,7 +94,11 @@ def calc_contdwn_responces(subject, region=None, band=[40, 100], timebin_sec=0.5
     for path in paths:
         event_reader_obj =  event_reader(path['events'])
         cntdwns = event_reader_obj.get_countdowns()
-        if len(cntdwns) > avg + in_session_gap:
+        # PATCH
+        admit_list = np.argwhere([e['onset sample'] > 500 for e in cntdwns]).squeeze()
+        cntdwns = [cntdwns[i] for i in admit_list]
+        #
+        if len(cntdwns) >= 26:#avg + in_session_gap:
             cntdwn_list.append(cntdwns)
     if len(cntdwn_list) < 2:
         rc = 'not enough valid sessions for {} {}'.format(subject, group)
@@ -154,7 +159,7 @@ def calc_contdwn_responces(subject, region=None, band=[40, 100], timebin_sec=0.5
         fig, ax = plt.subplots(2, 4, figsize=(15, 10), num='calc_contdwn_responces')
         fig.clf()
         fig, ax = plt.subplots(2, 4, figsize=(15, 10), num='calc_contdwn_responces')
-        fig.suptitle(subject)
+        #fig.suptitle(subject)
         for i_ses in range(4):
             for i_pos in range(2):
                 if i_ses < len(desc_list):
@@ -172,6 +177,8 @@ def calc_contdwn_responces(subject, region=None, band=[40, 100], timebin_sec=0.5
 
 
 
+
+
 base_folder = 'E:/ds004789-download'
 subjects_list = get_subject_list(base_folder=base_folder)
 
@@ -184,21 +191,88 @@ if False:
     assert False
 
 # parameters for the protocol
-region = 'precuneus'
-side = 'L'
-subject_list = ['1355T', '1338T', '1337E', '1334T', '1323T', '1243T', '1331T', '1153T', '1134T', '1094T', '1108J', '1065J']
-subject_list = ['sub-R' + s for s in subject_list]
+regions = ['precuneus', 'cuneus', 'lateraloccipital', 'lingual']
+sides = ['L', 'both', 'L', 'both']
+subject_list = ['sub-R1425D', 'sub-R1355T', 'sub-R1346T', 'sub-R1338T', 'sub-R1334T', 'sub-R1161E', 'sub-R1156D', 'sub-R1154D', 'sub-R1153T',
+                'sub-R1123C', 'sub-R1094T', 'sub-R1145J', 'sub-R1108J', 'sub-R1092J', 'sub-R1077T', 'sub-R1065J', 'sub-R1299T']
+# subject_list = ['1355T', '1338T', '1337E', '1334T', '1323T', '1243T', '1331T', '1153T', '1134T', '1094T', '1108J', '1065J']
+# subject_list = ['sub-R' + s for s in subject_list]
+mode = 'bipolar'# 'monopolar'#
+avg_depth = 6
 
 logfile_name = os.path.join(base_folder, 'logfile.txt')
 logfile_fd = open(logfile_name, 'wt')
 
 for subject in subject_list:
-    rc, data = calc_contdwn_responces(subject, region=[region, side], scan_files_only=False)
+    rc, data = calc_contdwn_responces(subject, regions=regions, sides=sides, mode=mode, scan_files_only=False, avg=avg_depth)
     print(subject, rc)
     logfile_fd.write(rc + '\n')
     if data is not None:
-        fname = os.path.join(base_folder, 'plots', subject + '.png')
+        data['fig'].suptitle(subject + '\navg={}'.format(avg_depth))
+        fname = os.path.join(base_folder, 'plots', subject + '_' + mode + '_raw_avgs_' + str(avg_depth) + '.png')
         data['fig'].savefig(fname)
+        #
+        #
+        #
+        #
+        # visualize
+        nchans, nses = data['p'][0][0].shape[0], len(data['p'])
+        num_lags = 4 # how many time taps to visualize
+        vectors = np.zeros((2, nses, nchans, num_lags))
+        #
+        fig, ax = plt.subplots(2, num_lags, num='pierson', figsize=(15, 10))
+        fig.clf()
+        fig, ax = plt.subplots(2, num_lags, num='pierson', figsize=(15, 10))
+        for i_lag in range(num_lags):
+            ax[0, i_lag].grid(True)
+            ax[0, i_lag].set_ylim([0.5, 3])
+            ax[0, i_lag].set_title('{:2.1f} - {:2.1f} sec.'.format(i_lag / 2, (i_lag + 1) / 2))
+        #
+        for i_ses, (p_ses, sem_ses) in enumerate(zip(data['p'], data['sem'])):
+            for i_rpt in range(2):
+                # normalize the data
+                nf = np.tile(p_ses[i_rpt][:, 0], (p_ses[i_rpt].shape[1], 1)).T
+                # p_ses[i_rpt] /= nf
+                # sem_ses[i_rpt] /= nf
+                vectors[i_rpt, i_ses] = p_ses[i_rpt][:, 1:num_lags+1] / nf[:, 1:num_lags+1]
+                for i_lag in range(num_lags):
+                    ax[0, i_lag].plot(vectors[i_rpt, i_ses, :, i_lag], label='s{}r{}'.format(i_ses+1, i_rpt+1))
+        # pierson correlation
+
+        n1, n2, nchans, nlags = vectors.shape
+        ur_vectors = vectors.reshape(n1*n2, nchans, nlags)
+        pc = np.zeros((n1*n2, n1*n2))
+        for i_lag in range(nlags):
+            tap_vectors = ur_vectors[:, :, i_lag]
+            mu = tap_vectors.mean(axis=0)
+            ax[0, i_lag].plot(mu, ':', linewidth=2, label='avg')
+            ax[0, i_lag].legend()
+            # plain covariance
+            c = (tap_vectors - mu) @ (tap_vectors - mu).T
+            sigma = np.sqrt(np.diag(c))
+            for i in range(c.shape[0]):
+                c[:, i] /= sigma[i]
+                c[i, :] /= sigma[i]
+            # pairwise pierson
+            for i1 in range(n1*n2):
+                for i2 in range(n1*n2):
+                    tap_vector_ij = tap_vectors[(i1, i2), :]
+                    tap_vector_ij = tap_vector_ij * (tap_vector_ij < 4) # PATCH!!! FOR HIGH LEVEL NOISES
+                    muij = tap_vector_ij.mean(axis=1)
+                    dij = tap_vector_ij - muij.reshape(2, 1)
+                    pc[i1, i2] = (dij[0].reshape(1, nchans) @ dij[1].reshape(nchans, 1)).squeeze() / (np.linalg.norm(dij[0]) * np.linalg.norm(dij[1]) + 1e-12)
+                    # pc[i1, i2] = np.corrcoef(tap_vector_ij)[0, 1]
+
+            sns.heatmap(np.round(pc, decimals=2), ax=ax[1, i_lag], vmin=-1, vmax=1, annot=True, cbar=False, square=True)
+
+        fig.suptitle('PAIRWISE pearson correlation    ' + subject + '\navg={}'.format(avg_depth))
+        fname = os.path.join(base_folder, 'plots', subject + '-' + mode + '_pearson' + str(avg_depth) + '.png')
+        fig.savefig(fname)
+        plt.show(block=False)
+        plt.pause(0.25)
+
+
+
 
 logfile_fd.close()
 
