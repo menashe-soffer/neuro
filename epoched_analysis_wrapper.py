b379dd2ddad52e4370082dcbbe56073364df8235
menashe soffer <menashe.soffer.23@gmail.com>
2025-03-25 17:04:54 +0200

    arrange code (wrap p_values and plot in a function), save p_values to fil

--- epoched_analysis_wrapper.py
+++ epoched_analysis_wrapper.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import scipy
+import pickle
+import tqdm
+
 #import glob
 import mne
 
@@ -112,8 +115,12 @@ def normalize_all_epochs(epoched, baseline_boundaries=(-0.5, -0.05)):
     num_epochs, num_chans, num_epoch_samps = epoched._data.shape
     for i_epoch in range(num_epochs):
         for i_chan in range(num_chans):
-            nf = np.norm(epoched._data[i_epoch, i_chan] * mask) / mask.sum()
-            epoched._data[i_epoch, i_chan] /= nf
+            nf = np.linalg.norm(epoched._data[i_epoch, i_chan] * mask) / np.sqrt(mask.sum())
+            #print(i_epoch, i_chan, nf)
+            if nf > 0:
+                epoched._data[i_epoch, i_chan] /= (nf + 1e-32)
+            else:
+                epoched._data[i_epoch, i_chan, :] = 0
 
     return
 
@@ -137,12 +144,59 @@ def my_wilcoxon(x, y):
     z = np.abs(Ty - Ety) / s
     return np.exp(-z/2)
 
+
+def calculate_p_values(evoked, pre_intvl=[-0.5, -0.1], post_intval=[0.1, 0.6],
+                       display_post_cursor=6, display_seperator_pitch=None, display_title=None, show=True):
+
+    p_values = np.zeros(len(evoked.ch_names))
+    increase_mask = np.zeros(len(evoked.ch_names), dtype=bool)
+    pre_mask = (evoked.times > pre_intvl[0]) * (evoked.times < pre_intvl[-1])
+    post_mask = (evoked.times > post_intval[0]) * (evoked.times < post_intval[-1])
+    for i_ch in range(len(evoked.ch_names)):
+        evoked._data[i_ch] = np.log(np.convolve(np.ones(8) / 8, evoked._data[i_ch], mode='same') + 1e-9)
+        increase_mask[i_ch] = np.sign(evoked._data[i_ch][post_mask].mean() - evoked._data[i_ch][pre_mask].mean()) > 0
+        p_values[i_ch] = my_wilcoxon(evoked._data[i_ch][pre_mask], evoked._data[i_ch][post_mask])
+
+    figs = []
+    if show:
+        response_mask = increase_mask * (p_values < 0.05)  # log_p_thd
+        for i_ch in range(len(evoked.ch_names)):
+            i_ax = i_ch % 20
+            if i_ax == 0:
+                fig, ax = plt.subplots(4, 5, figsize=(12, 8), sharex=True, sharey=True)
+                fig.suptitle(epoched_file)
+                figs.append(fig)
+            ax.flatten()[i_ax].plot(evoked.times[150:-150], evoked._data[i_ch][150:-150],
+                                    linewidth=0.5 if evoked.ch_names[i_ch] in evoked.info['bads'] else 2,
+                                    c='r' if response_mask[i_ch] else 'b')
+            ax.flatten()[i_ax].plot(evoked.times[pre_mask], evoked._data[i_ch][pre_mask],
+                                    linewidth=0.5 if evoked.ch_names[i_ch] in evoked.info['bads'] else 4,
+                                    c='r' if response_mask[i_ch] else 'b')
+            ax.flatten()[i_ax].plot(evoked.times[post_mask], evoked._data[i_ch][post_mask],
+                                    linewidth=0.5 if evoked.ch_names[i_ch] in evoked.info['bads'] else 4,
+                                    c='r' if response_mask[i_ch] else 'b')
+            ax.flatten()[i_ax].set_ylabel(evoked.ch_names[i_ch])
+            # [ax.flatten()[i_ax].plot([x, x], (-1, 1), c='r') for x in np.arange(10)] # for CNTDWV
+            [ax.flatten()[i_ax].plot([x, x], (-1, 1), c='r') for x in np.arange(start=0, stop=display_post_cursor, step=display_seperator_pitch)]  # for LIST
+            ax.flatten()[i_ax].set_ylim((-0.5, 0.75))
+            # ax.flatten()[i_ax].set_xlim((-0.8, 6)) # for CNTDWV
+            ax.flatten()[i_ax].set_xlim((-0.8, display_post_cursor))  # for LIST
+            ax.flatten()[i_ax].set_xlabel('p value : {:7.4f}'.format(p_values[i_ch]))
+            ax.flatten()[i_ax].grid(True)
+
+    return p_values, increase_mask, figs
+
+
+
+
+
 if __name__ == '__main__':
 
-    file_type, event_type = 'list', 'LIST'# 'cntdwn', 'CNTDWN'#
+    SHOW = False
+    file_type, event_type, sub_event_type = 'list', 'LIST', 'WORD'# 'cntdwn', 'CNTDWN'#
     list = find_epoched_subject(base_folder=base_folder, epoched_folder=epoched_folder, type=file_type)
-    print(list)
-    for subject_item in list:
+    #print(list)
+    for subject_item in tqdm.tqdm(list):
         for epoched_file in subject_item['epoched']:
             epoched = mne.read_epochs(epoched_file, verbose=False)
             #cmplx_signal = epoched.apply_hilbert().get_data()
@@ -153,12 +207,11 @@ if __name__ == '__main__':
             epoch_mask = np.zeros(len(annotations), dtype=bool)
             for i_epoch in range(len(annotations)):
                 annot_type = [a[2] for a in annotations[i_epoch]]
-                print(annot_type)
+                #print(annot_type)
                 is_cntdwn = np.any([a == event_type for a in annot_type])
                 is_bad = np.any([a.find('bad_') > -1 for a in annot_type])
                 epoch_mask[i_epoch] = (len(annot_type) > 0) and (is_cntdwn) and (not is_bad)
 
-            #cmplx_signal = cmplx_signal[epoch_mask][:, ch_mask]
             epoched_gamma = apply_gamma_responces(epoched=epoched, epoch_mask=epoch_mask)
 
             evoked = epoched_gamma.average('data')
@@ -166,66 +219,22 @@ if __name__ == '__main__':
             for i_chan in range(evoked._data.shape[0]):
                 nf = np.linalg.norm(evoked._data[i_chan][norm_mask]) / np.sqrt(norm_mask.sum())
                 evoked._data[i_chan] /= nf
-            # fig = evoked.plot()
-            # plt.show()
-            #
-            # for i_ch in range(len(evoked.ch_names)):
-            #     i_ax = i_ch % 20
-            #     if i_ax == 0:
-            #         fig, ax = plt.subplots(4, 5, figsize=(12, 8), sharex=True, sharey=True)
-            #     ax.flatten()[i_ax].plot(evoked.times[150:-150], evoked._data[i_ch][150:-150],
-            #                             linewidth=0.5 if evoked.ch_names[i_ch] in evoked.info['bads'] else 2)
-            #     ax.flatten()[i_ax].set_ylabel(evoked.ch_names[i_ch])
-            #     [ax.flatten()[i_ax].plot([x, x], (0.75, 1.25), c='r') for x in np.arange(10)]
-            #     ax.flatten()[i_ax].set_ylim((0.5, 1.8))
-            #     ax.flatten()[i_ax].set_xlim((-0.8, 6))
-            #     ax.flatten()[i_ax].grid(True)
-            # plt.show()
-            #
-            p_values = np.zeros(len(evoked.ch_names))
-            increase_mask = np.zeros(len(evoked.ch_names), dtype=bool)
-            response_mask = np.zeros(len(evoked.ch_names), dtype=bool)
-            log_p_thd = -10
-            pre_mask = (evoked.times > -0.5) * (evoked.times < -0.1)
-            post_mask = (evoked.times > 0.1) * (evoked.times < 0.6)
-            for i_ch in range(len(evoked.ch_names)):
-                evoked._data[i_ch] = np.log(np.convolve(np.ones(8)/8, evoked._data[i_ch], mode='same') + 1e-9)
-                #_, p_values[i_ch] = scipy.stats.wilcoxon(evoked._data[i_ch][pre_mask], evoked._data[i_ch][post_mask])
-                increase_mask[i_ch] = np.sign(evoked._data[i_ch][post_mask].mean() - evoked._data[i_ch][pre_mask].mean()) > 0
-                p_values[i_ch] = my_wilcoxon(evoked._data[i_ch][pre_mask], evoked._data[i_ch][post_mask])
-            response_mask = increase_mask * (p_values < 0.05)#log_p_thd
-            for i_ch in range(len(evoked.ch_names)):
-                i_ax = i_ch % 20
-                if i_ax == 0:
-                    fig, ax = plt.subplots(4, 5, figsize=(12, 8), sharex=True, sharey=True)
-                    fig.suptitle(epoched_file)
-                ax.flatten()[i_ax].plot(evoked.times[150:-150], evoked._data[i_ch][150:-150],
-                                        linewidth=0.5 if evoked.ch_names[i_ch] in evoked.info['bads'] else 2,
-                                        c='r' if response_mask[i_ch] else 'b')
-                ax.flatten()[i_ax].set_ylabel(evoked.ch_names[i_ch])
-                #[ax.flatten()[i_ax].plot([x, x], (-1, 1), c='r') for x in np.arange(10)] # for CNTDWV
-                [ax.flatten()[i_ax].plot([2.6 * x, 2.6 * x], (-1, 1), c='r') for x in np.arange(5)] # for LIST
-                ax.flatten()[i_ax].set_ylim((-0.5, 0.75))
-                #ax.flatten()[i_ax].set_xlim((-0.8, 6)) # for CNTDWV
-                ax.flatten()[i_ax].set_xlim((-0.8, 10)) # for LIST
-                ax.flatten()[i_ax].set_xlabel('p value : {:7.4f}'.format(p_values[i_ch]))
-                ax.flatten()[i_ax].grid(True)
-            #plt.show()
 
+
+            p_values, increase_mask, _ = calculate_p_values(evoked, pre_intvl=[-0.5, -0.1], post_intval=[0.1, 0.6],
+                                                            display_post_cursor=10, display_seperator_pitch=2.6, display_title=epoched_file, show=SHOW)
+            with open (os.path.join(os.path.dirname(epoched_file), 'p_values_' + event_type), 'wb') as fd:
+                pickle.dump(dict({'ch_names': ch_names, 'p_values': p_values, 'increase_mask': increase_mask}), fd)
             #
             # sub event
-            sub_event_type = 'WORD'
             mask = np.array([d == event_type for d in epoched.annotations.description]).flatten()
             events_for_sub_epoching = np.zeros((mask.sum(), 3))
             events_for_sub_epoching[:, 0] = epoched.annotations.onset[mask]
             durations = epoched.annotations.duration[mask].max()
-            #mne.Epochs(epoched, events=events_for_sub_epoching, tmin=-1, tmax=durations + 1, reject_by_annotation=False)
 
             fs = epoched_gamma.info['sfreq']
             epoched_data = epoched_gamma.get_data()
             major_duration = (epoched_data.shape[-1] - 1) / fs
-            #flattened_len = epoched_data.shape[-1] * len(annotations)
-            #flattened_time = np.arange(flattened_len) / fs
             base_time = 0
             time_shift = -epoched_gamma.times[0]
             semi_raw = mne.io.RawArray(np.concatenate(epoched_data, axis=1), epoched.info, verbose=False)
@@ -240,47 +249,22 @@ if __name__ == '__main__':
             events_for_epoching[:, 0] = (semi_raw.annotations.onset * fs).astype(int)
             events_for_epoching[:, 2] = 0
             # BIG TBD: KKEP BAD EVENTS, ASSIGN EVENT TYPES TO DISTINGUISH
-            sub_epoched = mne.Epochs(semi_raw, events=events_for_epoching, tmin=-1, tmax=sub_events_inner[:, 1].max() + 1, baseline=None)
+            sub_epoched = mne.Epochs(semi_raw, events=events_for_epoching, tmin=-1, tmax=sub_events_inner[:, 1].max() + 1, baseline=None, preload=True)
+            normalize_all_epochs(sub_epoched)
             sub_evoked = sub_epoched.average('data')
-            # #
-            # fig, ax = plt.subplots(2, 1)
-            # ax[0].plot(semi_raw.get_data()[0][750:5750])
-            # ax[0].grid(True)
-            # ax[0].plot(sub_epoched.get_data()[0, 0] + 0)
-            # ax[1].grid(True)
-            # plt.show()
             #
             norm_mask = (sub_evoked.times >= -0.5) * (sub_evoked.times <= -0.05)
             for i_chan in range(sub_evoked._data.shape[0]):
                 nf = np.linalg.norm(sub_evoked._data[i_chan][norm_mask]) / np.sqrt(norm_mask.sum())
                 sub_evoked._data[i_chan] /= nf
             #
-            p_values = np.zeros(len(sub_evoked.ch_names))
-            increase_mask = np.zeros(len(sub_evoked.ch_names), dtype=bool)
-            response_mask = np.zeros(len(sub_evoked.ch_names), dtype=bool)
-            pre_mask = (sub_evoked.times > -0.4) * (sub_evoked.times < -0.1)
-            post_mask = (sub_evoked.times > 0.1) * (sub_evoked.times < 0.5)
-            for i_ch in range(len(sub_evoked.ch_names)):
-                sub_evoked._data[i_ch] = np.log(np.convolve(np.ones(8)/8, sub_evoked._data[i_ch], mode='same') + 1e-9)
-                #_, p_values[i_ch] = scipy.stats.wilcoxon(evoked._data[i_ch][pre_mask], evoked._data[i_ch][post_mask])
-                increase_mask[i_ch] = np.sign(sub_evoked._data[i_ch][post_mask].mean() - sub_evoked._data[i_ch][pre_mask].mean()) > 0
-                p_values[i_ch] = my_wilcoxon(sub_evoked._data[i_ch][pre_mask], sub_evoked._data[i_ch][post_mask])
-            response_mask = increase_mask * (p_values < 0.05)#log_p_thd
-            for i_ch in range(len(sub_evoked.ch_names)):
-                i_ax = i_ch % 20
-                if i_ax == 0:
-                    fig, ax = plt.subplots(4, 5, figsize=(12, 8), sharex=True, sharey=True)
-                    fig.suptitle(epoched_file)
-                ax.flatten()[i_ax].plot(sub_evoked.times[150:-150], sub_evoked._data[i_ch][150:-150],
-                                        linewidth=0.5 if sub_evoked.ch_names[i_ch] in sub_evoked.info['bads'] else 2,
-                                        c='r' if response_mask[i_ch] else 'b')
-                ax.flatten()[i_ax].set_ylabel(sub_evoked.ch_names[i_ch])
-                [ax.flatten()[i_ax].plot([2.6*x, 2.6*x], (-1, 1), c='r') for x in np.arange(5)]
-                ax.flatten()[i_ax].set_ylim((-0.5, 0.75))
-                ax.flatten()[i_ax].set_xlim((-0.8, 10))
-                ax.flatten()[i_ax].set_xlabel('p value : {:7.4f}'.format(p_values[i_ch]))
-                ax.flatten()[i_ax].grid(True)
-            plt.show()
+
+            p_values, increase_mask, _ = calculate_p_values(sub_evoked, pre_intvl=[-0.4, -0.1], post_intval=[0.1, 0.4],
+                                                            display_post_cursor=3, display_seperator_pitch=2.6, display_title=epoched_file, show=SHOW)
+            with open (os.path.join(os.path.dirname(epoched_file), 'p_values_' + sub_event_type), 'wb') as fd:
+                pickle.dump(dict({'ch_names': ch_names, 'p_values': p_values, 'increase_mask': increase_mask}), fd)
+
+            if SHOW:
+                plt.show()
 
 
-            plt.show()
